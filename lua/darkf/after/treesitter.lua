a5f8cf82025bae2f5f460956d8dd45fb2c3878af
Valentin Hassinen <darkfrobozz.mega@gmail.com>
2024-02-16 18:19:12 +0100

    fixed error with treesitter

--- lua/darkf/after/treesitter.lua
+++ lua/darkf/after/treesitter.lua
@@ -10,7 +10,7 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   -- List of parsers to ignore installing (or "all")
-  ignore_install = { "javascript" },
+  ignore_install = { },
 
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
@@ -38,7 +38,7 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-  require'nvim-treesitter.configs'.setup {
+  modules = {
 	  incremental_selection = {
 		  enable = true,
 		  keymaps = {
@@ -48,5 +48,17 @@ require'nvim-treesitter.configs'.setup {
 			  node_decremental = "<S-TAB>",
 		  },
 	  },
-  }
+	  highlight = {
+		  enable = true,
+		  custom_captures = {
+			  -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+			  ["foo.bar"] = "Identifier",
+		  },
+		  -- Setting this to true or a list of languages will run `:h syntax` and tree-sitter at the same time.
+		  additional_vim_regex_highlighting = false,
+	  },
+	  indent = {
+		  enable = true
+	  },
+  },
 }
